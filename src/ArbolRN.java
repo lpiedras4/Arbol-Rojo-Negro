c661982ad31386be10a91f27291e2c207550b44f
lpiedras4 <piedras.leo@gmail.com>
2025-11-09 20:56:41 -0800

    Se crearon las clases nodo, lista, cola y arbol rojo negro. Se añadieron los métodos de caso de uso

--- src/ArbolRN.java
+++ src/ArbolRN.java
@@ -1,3 +1,36 @@
-public class ArbolRN {
+public class ArbolRN <T> {
+private Node root;
+public ArbolRN (Node root){
+    this.root = root;
+}
+public void crearArbol (){
 
+}
+public void insertarNodo(T data){
+
+    }
+    public  void eliminarNodo(T data){
+
+    }
+    public void buscarNodo(T data){
+
+    }
+    public  void rotarIzq(T data){
+
+    }
+    public  void rotarDer(T data){
+
+    }
+    public Lista <T> preOrden (){
+    return null;
+    }
+    public Lista <T> entreOrden (){
+        return null;
+    }
+    public Lista <T> posOrden (){
+        return null;
+    }
+    public Lista <T> aLoAncho (){
+        return null;
+    }
 }
