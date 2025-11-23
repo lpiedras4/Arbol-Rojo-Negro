b9ba162c02e67f2525563ac02016cb2b6573e91c
lpiedras4 <piedras.leo@gmail.com>
2025-11-22 23:36:12 -0800

    Se agregaron archivos de controlador y se avanzó en la GUI principal

--- src/Lista.java
+++ src/Lista.java
@@ -1,84 +1,89 @@
-public class Lista <T> {
-    private Node head;
-    public Lista(){
-        head = null;
-    }
-    public  T get(int index){
-        if(head == null){
+public class Lista <T>{
+    private Node head = null;
+
+    public T get(int index) {
+        if (this.head == null) {
             System.out.println("Error: Elemento no encontrado");
             return null;
+        } else {
+            Node curr = this.head;
+
+            for(int j = 0; j < index; ++j) {
+                curr = curr.next;
+            }
+
+            return (T)curr.data;
         }
-        Node curr = head;
-        for(int j=0;j<index;j++){
-            curr = curr.next;
-        }
-        return (T) curr.data;
     }
-    public int size(){
+
+    public int size() {
         int size = 0;
-        Node curr = head;
-        while(curr != null){
-            size++;
-            curr = curr.next;
+
+        for(Node curr = this.head; curr != null; curr = curr.next) {
+            ++size;
         }
+
         return size;
     }
-    public Node <T> insert (T data){
-        Node newNode = new Node(data,"rojo");
+
+    public Node<T> insert(T data) {
+        Node newNode = new Node(data, "rojo");
         newNode.next = null;
-        if(head == null){
-            head = newNode;
-        }else{
-            Node last = head;
-            while(last.next!=null){
-                last = last.next;
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            Node last;
+            for(last = this.head; last.next != null; last = last.next) {
             }
 
             last.next = newNode;
         }
-        return head;
+
+        return this.head;
     }
 
-    public void printList (){
-        if(vacia()){
+    public void printList() {
+        if (this.vacia()) {
             System.out.println("Lista Vacia");
         }
-        Node current = head;
-        while(current != null){
+
+        for(Node current = this.head; current != null; current = current.next) {
             System.out.println(current.data);
-            current = current.next;
         }
+
     }
 
-    public Node <T> remove (int key){
-
-        if(head==null){
+    public Node<T> remove(int key) {
+        if (this.head == null) {
             System.out.println("List is empty");
-        }else{
-            Node currentNode = head, prev = null;
-            if(key==0){
-                head = currentNode.next;
+        } else {
+            Node currentNode = this.head;
+            Node prev = null;
+            if (key == 0) {
+                this.head = currentNode.next;
                 System.out.println("Found key " + key + " and deleted");
-                return head;
+                return this.head;
             }
-            for(int i=0;i<key;i++){
+
+            for(int i = 0; i < key; ++i) {
                 prev = currentNode;
                 currentNode = currentNode.next;
             }
 
-            if(currentNode!=null){
+            if (currentNode != null) {
                 prev.next = currentNode.next;
                 System.out.println("Found key " + key + " and deleted");
             }
 
-            if (currentNode == null){
+            if (currentNode == null) {
                 System.out.println(key + " not found");
             }
         }
 
-        return head;
+        return this.head;
     }
-    public boolean vacia(){
-        return head == null;
+
+    public boolean vacia() {
+        return this.head == null;
     }
 }
