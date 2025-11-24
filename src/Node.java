2b17b5a30371217cdfcb1ca2faabccb889daadfd
Yuyuu1819 <yuliettenunez@gmail.com>
2025-11-24 08:38:19 -0800

    Backend con rotaciones, busqueda, insercion, update de atributos listos. Eliminacion pendiente

--- src/Node.java
+++ src/Node.java
@@ -1,15 +1,10 @@
 public class Node <T>{
     T data;
-    Node next;
-    Node left;
-    Node right;
+    Node<T> next, left, right, parent;
     String color;
-    public Node (T data, String color){
+    public Node (T data){
         this.data = data;
-        this.color = color;
-        next = null;
-        left = null;
-        right = null;
+        this.color = "ROJO"; //siempre inicializados como rojos
     }
 
 }
