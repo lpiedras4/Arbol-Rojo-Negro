184ed20a4d74faf4475c344ed2bf37f056318ff7
Yuyuu1819 <yuliettenunez@gmail.com>
2025-11-24 09:01:44 -0800

    se creo branch yuliette con backend de rotaciones, inserciones, insert. elimination pendiente

--- src/main.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-import java.awt.*;
-
-public class main {
-    public static void main(String[] args) {
-        VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
-
-    }
-}
