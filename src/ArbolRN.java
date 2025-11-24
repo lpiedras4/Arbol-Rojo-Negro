5046ab29ed4e9aa31bf0882b132876296fb8485e
lpiedras4 <piedras.leo@gmail.com>
2025-11-23 22:22:54 -0800

    Se empezó con implementación de arbol rojo negro[

--- src/ArbolRN.java
+++ src/ArbolRN.java
@@ -6,13 +6,35 @@ public ArbolRN (Node root){
 public void crearArbol (){
 
 }
-public void insertarNodo(T data){
-
+    public void insertarNodo(T data){
+    Node <T> curr = new Node<>(data,"rojo");
+    Node <T> nodoEncontrado = busqRecurs(root,data);
+    if (nodoEncontrado==null){
+    nodoEncontrado = nodoEncontrado.getPadre();
+    }
     }
     public  void eliminarNodo(T data){
 
     }
-    public void buscarNodo(T data){
+    public Node<T> buscarNodo(T data){
+    Node <T> nodoEncontrado = busqRecurs(root,data);
+    balancear(nodoEncontrado);
+    return nodoEncontrado;
+    }
+    private Node <T> busqRecurs(Node nodo, T valor){
+    if(valor == nodo.getData()){
+        return nodo;
+    }else if((int)valor< (int)nodo.getData()){
+    if (nodo.getLeftChild()==null){
+        return nodo;
+    }
+    return busqRecurs(nodo.getLeftChild(),valor);
+    }else{
+        if(nodo.getRightChild()==null){
+            return nodo;
+        }
+        return busqRecurs(nodo.getRightChild(),valor);
+    }
 
     }
     public  void rotarIzq(T data){
