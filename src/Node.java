f908bcbcc317731dd79ebeb1281e75e2de540ae3
lpiedras4 <piedras.leo@gmail.com>
2025-11-25 22:07:05 -0800

    Se borraron metodos del controlador porque nico ya se encargó de eso

--- src/Node.java
+++ src/Node.java
@@ -1,10 +1,11 @@
 public class Node <T>{
-    T data;
-    Node<T> next, left, right, parent;
-    String color;
-    public Node (T data){
-        this.data = data;
-        this.color = "ROJO"; //siempre inicializados como rojos
-    }
+        T data;
+        Node<T> next, left, right, parent;
+        String color;
+        public Node (T data){
+            this.data = data;
+            this.color = "ROJO"; //siempre inicializados como rojos
+        }
+
 
 }
