83eb21145a8ae2ff305caddf181c8c37323171a8
lpiedras4 <piedras.leo@gmail.com>
2025-11-27 22:53:42 -0800

    Movio clase Node a archivo java propio

---
+++ src/Node.java
@@ -0,0 +1,17 @@
+public class Node<T extends Comparable<T>> {
+    T value;
+    Node<T> left, right, parent;
+    boolean color; // true = Rojo, false = Negro
+
+    // Para uso visual (se rellenan con computeNodePositions)
+    double x, y; //x y y se establecen para establecer las coordenadas que se usaran para dibujar el arbol.
+
+    Node(T value) {
+        this.value = value;
+        this.left = this.right = this.parent = null;
+        this.color = true; // nuevo nodo por defecto ROJO
+    }
+
+    boolean esRojo() { return color; }//color == true rojo
+    boolean esNegro() { return !color; } //color != true negro
+}
